3ac700e5ac469213c26b6b25a9d0a8d1d638bdc3
Lucas Gould <gould.lucas12@gmail.com>
2019-07-10 11:21:21 -0500

    Login no longer looks terrible

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="card" style="margin-right: 20px; margin-left: 20px;">
-        <div class="card-body">
-            <input class="form-control" type="text" v-model="email" placeholder="Email" style="margin: 3px">
-            <input class="form-control" type="password" v-model="password" placeholder="Password" style="margin: 3px">
-            <button @click="authenticate" class="btn btn-dark" style="margin: 3px">Log In</button>
-            <button @click="logout" class="btn btn-dark" style="margin: 3px">Log Out</button>
-        </div>
+    <div class="card mx-auto" style="width: 18rem;">
+      <div class="card-header" >
+        Login
+      </div>
+      <div class="card-body">
+        <input class="form-control" type="text" v-model="email" placeholder="Email" style="margin: 3px">
+        <input class="form-control" type="password" v-model="password" placeholder="Password" style="margin: 3px">
+        <button @click="authenticate" class="btn btn-dark" style="margin: 3px">Log In</button>
+        <button @click="logout" class="btn btn-dark" style="margin: 3px">Log Out</button>
+      </div>
     </div>
 </template>
 
